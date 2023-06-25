a9fcc220f3f850a8651175850d78e55a627b9eb2
Douglas <Douglas@192.168.15.108>
2023-06-25 11:59:55 -0300

    utf-8

--- src/view/RegistrationCepScreen.java
+++ src/view/RegistrationCepScreen.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -202,7 +203,7 @@ public class RegistrationCepScreen extends InternalFrame {
 				URL url = new URL("https://viacep.com.br/ws/" + txtInfoCep.getText().replace("-", "") + "/json/");
 
 				BufferedReader br = new BufferedReader(
-						new InputStreamReader(url.openConnection().getInputStream(), "UTF-8"));
+						new InputStreamReader(url.openConnection().getInputStream(), StandardCharsets.UTF_8));
 
 				String cepAux = "";
 				StringBuilder jsonCep = new StringBuilder();
