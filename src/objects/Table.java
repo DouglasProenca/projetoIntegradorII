45a90ec81ecea963681c45f4453009e7251d6575
Douglas <Douglas@192.168.15.109>
2023-10-01 11:01:26 -0300

    Impedindo de reordenar as colunas da tabela

--- src/objects/Table.java
+++ src/objects/Table.java
@@ -8,16 +8,16 @@ public class Table extends JTable {
 	
 	public Table(String [] columns, int rows) {
 		super(new TableModel(columns, rows));
+		this.getTableHeader().setReorderingAllowed (false);
 	}
 	
 	public Table(String[] colunas, int rows, boolean[] canEdit) {
 		super(new TableModel(colunas,0,canEdit));
+		this.getTableHeader().setReorderingAllowed (false);
 	}
 
 	@Override
 	public DefaultTableModel getModel() {
 		return (DefaultTableModel) super.getModel();
 	}
-
-	
 }
